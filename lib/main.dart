993d2db6a88dd6342e852949cd8e5342fc12b687
Quoc-Thong212 <maiquocthong212@gmail.com>
2025-10-21 23:15:01 +0700

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fashion_app/viewmodels/employee_role_viewmodel.dart';
 import 'package:fashion_app/viewmodels/storestaff_viewmodel.dart';
+import 'package:fashion_app/views/login/login_screen.dart';
 import 'package:fashion_app/views/shop/test_login.dart';
 import 'package:fashion_app/views/user/userprofile_screen.dart';
 import 'package:fashion_app/views/admin_confirm_screen.dart';
@@ -7,7 +8,6 @@ import 'package:fashion_app/views/admin_home_screen.dart';
 import 'package:fashion_app/views/admin_manageShop_screen.dart';
 import 'package:fashion_app/views/admin_shopAccount_screeen.dart';
 import 'package:fashion_app/viewmodels/shop_viewmodel.dart';
-import 'package:fashion_app/views/register_screen.dart';
 import 'package:fashion_app/views/shop/shop_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     
-    return Scaffold(body: Center(child: ShopScreen()));
+    return Scaffold(body: Center(child: LoginScreen()));
     //return Scaffold(body: Center(child: AdminHomeScreen()));
   }
 }
